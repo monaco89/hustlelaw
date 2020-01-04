657a9eb3d555c3c3c4abc4690bc0d975866d35a4
monaco89 <nick.monaco15@gmail.com>
2020-01-04 10:47:11 -0500

    wrap article around clients

--- src/components/clients.js
+++ src/components/clients.js
@@ -1,53 +1,55 @@
 import React from "react"
 
-// import client1 from "../images/clients/brashful.png"
-// import client2 from "../images/clients/lacallefb.png"
-// import client3 from "../images/clients/megafanslogo.jpg"
-// import client4 from "../images/clients/shv.jpg"
+import client1 from "../images/clients/brashful.png"
+import client2 from "../images/clients/lacallefb.png"
+import client3 from "../images/clients/megafanslogo.jpg"
+import client4 from "../images/clients/shv.jpg"
 
 const Clients = () => (
-  <div className="row">
-    <div className="col">
-      <div
-        style={{
-          padding: "1rem 0",
-          textAlign: "center",
-        }}
-      >
-        {/* <img src={client1} style={{ height: "280px" }} /> */}
+  <article>
+    <div className="row">
+      <div className="col">
+        <div
+          style={{
+            padding: "1rem 0",
+            textAlign: "center",
+          }}
+        >
+          <img src={client1} style={{ height: "280px" }} />
+        </div>
+      </div>
+      <div className="col">
+        <div
+          style={{
+            padding: "1rem 0",
+            textAlign: "center",
+          }}
+        >
+          <img src={client2} style={{ height: "280px" }} />
+        </div>
+      </div>
+      <div className="col">
+        <div
+          style={{
+            padding: "1rem 0",
+            textAlign: "center",
+          }}
+        >
+          <img src={client3} style={{ height: "280px" }} />
+        </div>
+      </div>
+      <div className="col">
+        <div
+          style={{
+            padding: "1rem 0",
+            textAlign: "center",
+          }}
+        >
+          <img src={client4} style={{ height: "280px" }} />
+        </div>
       </div>
     </div>
-    <div className="col">
-      <div
-        style={{
-          padding: "1rem 0",
-          textAlign: "center",
-        }}
-      >
-        {/* <img src={client2} style={{ height: "280px" }} /> */}
-      </div>
-    </div>
-    <div className="col">
-      <div
-        style={{
-          padding: "1rem 0",
-          textAlign: "center",
-        }}
-      >
-        {/* <img src={client3} style={{ height: "280px" }} /> */}
-      </div>
-    </div>
-    <div className="col">
-      <div
-        style={{
-          padding: "1rem 0",
-          textAlign: "center",
-        }}
-      >
-        {/* <img src={client4} style={{ height: "280px" }} /> */}
-      </div>
-    </div>
-  </div>
+  </article>
 )
 
 export default Clients
